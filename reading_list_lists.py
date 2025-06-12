e6fbbd82e554b816f9aaddca4799744adb39599b
IvaSteb <bogomolica11@gmail.com>
2025-06-12 22:40:13 +0200

    Create reading_list_lists.py

---
+++ reading_list_lists.py
@@ -0,0 +1,10 @@
+# Write code below 💖
+
+books = ['Harry Potter', '1984', 'The Fault in Our Stars', 'The Mom Test', 'Life in Code']
+
+books.append('Pachinko')
+
+books.remove('1984')
+books.pop(2)
+
+print(books)
